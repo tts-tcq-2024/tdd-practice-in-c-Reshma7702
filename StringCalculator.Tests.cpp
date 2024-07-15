35d232f781632936097e0501a7d3e18dd86888f2
Reshma7702 <reshmabuddy7702@gmail.com>
2024-07-15 18:28:23 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -1,7 +1,6 @@
 #include <gtest/gtest.h>
 #include "StringCalculator.h"
 
-// Test when the input is empty
 TEST(StringCalculatorAddTests, ExpectZeroForEmptyInput) {
     int expectedresult = 0;
     const char* input = "";
@@ -9,7 +8,6 @@ TEST(StringCalculatorAddTests, ExpectZeroForEmptyInput) {
     ASSERT_EQ(result, expectedresult);
 }
 
-// Test when the input is a single zero
 TEST(StringCalculatorAddTests, ExpectZeroForSingleZero) {
     int expectedresult = 0;
     const char* input = "0";
@@ -17,7 +15,6 @@ TEST(StringCalculatorAddTests, ExpectZeroForSingleZero) {
     ASSERT_EQ(result, expectedresult);
 }
 
-// Test when the input is two numbers separated by comma
 TEST(StringCalculatorAddTests, ExpectSumForTwoNumbers) {
     int expectedresult = 3;
     const char* input = "1,2";
@@ -25,7 +22,6 @@ TEST(StringCalculatorAddTests, ExpectSumForTwoNumbers) {
     ASSERT_EQ(result, expectedresult);
 }
 
-// Test when the input contains newline as a delimiter
 TEST(StringCalculatorAddTests, ExpectSumWithNewlineDelimiter) {
     int expectedresult = 6;
     const char* input = "1\n2,3";
@@ -33,7 +29,6 @@ TEST(StringCalculatorAddTests, ExpectSumWithNewlineDelimiter) {
     ASSERT_EQ(result, expectedresult);
 }
 
-// Test when the input contains numbers greater than 1000
 TEST(StringCalculatorAddTests, IgnoreNumbersGreaterThan1000) {
     int expectedresult = 1;
     const char* input = "1,1001";
@@ -41,15 +36,17 @@ TEST(StringCalculatorAddTests, IgnoreNumbersGreaterThan1000) {
     ASSERT_EQ(result, expectedresult);
 }
 
-// Test when the input contains a custom delimiter
 TEST(StringCalculatorAddTests, ExpectSumWithCustomDelimiter) {
     int expectedresult = 3;
     const char* input = "//;\n1;2";
     int result = add(input);
     ASSERT_EQ(result, expectedresult);
 }
-
-// Test when the input contains a negative number
+TEST(StringCalculatorAddTests, ExpectSumWithCustomDelimiter_EmptyString) {
+    const char* input = "";
+    add(input);
+   
+}
 TEST(StringCalculatorAddTests, HandleNegativeNumbers) {
     const char* input = "-1,2";
     testing::internal::CaptureStdout();
@@ -57,67 +54,4 @@ TEST(StringCalculatorAddTests, HandleNegativeNumbers) {
     std::string output = testing::internal::GetCapturedStdout();
     ASSERT_EQ(result, -1);
     ASSERT_EQ(output, "Negatives not allowed: -1 \n");
-}
-
-// Test when the input contains multiple negative numbers
-TEST(StringCalculatorAddTests, HandleMultipleNegativeNumbers) {
-    const char* input = "-1,-2,3";
-    testing::internal::CaptureStdout();
-    int result = add(input);
-    std::string output = testing::internal::GetCapturedStdout();
-    ASSERT_EQ(result, -1);
-    ASSERT_EQ(output, "Negatives not allowed: -1 -2 \n");
-}
-
-// Test when the input contains custom delimiter with special regex characters
-TEST(StringCalculatorAddTests, CustomDelimiterWithRegexCharacters) {
-    int expectedresult = 6;
-    const char* input = "//.\n1.2.3";
-    int result = add(input);
-    ASSERT_EQ(result, expectedresult);
-}
-
-// Test when the input contains custom delimiter of length greater than 1
-TEST(StringCalculatorAddTests, CustomDelimiterWithLongLength) {
-    int expectedresult = 6;
-    const char* input = "//[***]\n1***2***3";
-    int result = add(input);
-    ASSERT_EQ(result, expectedresult);
-}
-
-// Test when the input contains multiple custom delimiters
-TEST(StringCalculatorAddTests, MultipleCustomDelimiters) {
-    int expectedresult = 6;
-    const char* input = "//[*][%]\n1*2%3";
-    int result = add(input);
-    ASSERT_EQ(result, expectedresult);
-}
-
-// Test when the input contains delimiters at the edge of the string
-TEST(StringCalculatorAddTests, DelimitersAtEdgeOfString) {
-    int expectedresult = 6;
-    const char* input = ",1,2,3,";
-    int result = add(input);
-    ASSERT_EQ(result, expectedresult);
-}
-
-// Test when the input contains empty sections between delimiters
-TEST(StringCalculatorAddTests, EmptySectionsBetweenDelimiters) {
-    int expectedresult = 6;
-    const char* input = "1,\n,2,\n,3";
-    int result = add(input);
-    ASSERT_EQ(result, expectedresult);
-}
-
-// Test for default delimiter branch coverage
-TEST(StringCalculatorAddTests, DefaultDelimiterBranchCoverage) {
-    const char* input = "1,2\n3";
-    int expectedresult = 6;
-    int result = add(input);
-    ASSERT_EQ(result, expectedresult);
-}
-
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
 }
